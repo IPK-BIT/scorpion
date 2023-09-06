5ee7b961b8aa6668e898e78675b4bf9c169b6292
feserm <feser@ipk-gatersleben.de>
2023-09-06 08:24:20 +0000

    changes extension import

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
-    'sphinxcontrib-httpdomain',
+    "sphinxcontrib.httpdomain",
 ]
 
 intersphinx_mapping = {
