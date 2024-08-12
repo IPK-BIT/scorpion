1edb7a952f6e7546399167bfee4d7b8b86dc6ff1
feserm <feser@ipk-gatersleben.de>
2024-08-12 11:41:10 +0000

    added banner for development instance

--- webui/src/routes/+layout.svelte
+++ webui/src/routes/+layout.svelte
@@ -6,6 +6,7 @@
 	import { onMount } from 'svelte';
 	import axios from 'axios';
 	import { goto } from '$app/navigation';
+	import { PUBLIC_DEV } from '$env/static/public';
 
 	onMount(async () => {
 		// get URL search params
@@ -54,6 +55,24 @@
 {:else}
 	<div class="app">
 		<Header />
+		{#if PUBLIC_DEV}
+		<div class="p-2">
+		<div role="alert" class="alert alert-error">
+			<svg
+			  xmlns="http://www.w3.org/2000/svg"
+			  class="h-6 w-6 shrink-0 stroke-current"
+			  fill="none"
+			  viewBox="0 0 24 24">
+			  <path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				stroke-width="2"
+				d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
+			</svg>
+			<span>Warning: Development Instance. There are no backups made and data can be deleted without further notice!</span>
+		  </div>
+		</div>
+		{/if}
 		<main>
 			<slot />
 		</main>
